8358e400ac886f9963664755ce6adcdbfed4474e
Quantum <mcleague@126.com>
2018-05-29 11:24:12 +0800

    修复了一些智障的问题

--- generateQst.php
+++ generateQst.php
@@ -10,7 +10,7 @@ function getArrayString($format, $array){
 function generatePhones($prefix, $format, $map){
 	$txt = '';
 	foreach($map as $key => $one){
-		$txt .= sprintf('QS "%s-Phone_%s %s', $prefix, $key, getArrayString($format, $one)) . "\r\n";
+		$txt .= sprintf('QS "%s-Phone_%s" %s', $prefix, $key, getArrayString($format, $one)) . "\r\n";
 	}
 	return $txt;
 }
@@ -18,7 +18,7 @@ function generatePhones($prefix, $format, $map){
 function generatePhoneList($prefix, $format, $map){
 	$txt = '';
 	foreach($map as $one){
-		$txt .= sprintf('QS "%s-Phone_%s %s', $prefix, $one, getArrayString($format, [$one])) . "\r\n";
+		$txt .= sprintf('QS "%s-Phone_%s" %s', $prefix, $one, getArrayString($format, [$one])) . "\r\n";
 	}
 	return $txt;
 }
@@ -38,14 +38,14 @@ $phoneMap = [
 ];
 $genMap = ['a', 'o', 'e', 'e2', 'i', 'u', 'v', 'i', 'i2', 'i3', 'er', 'ai', 'ei', 'ao', 'ou', 'ia', 'ie', 'ua', 'uo', 've', 'iao', 'iou', 'uai', 'uei', 'an', 'en', 'in', 'vn', 'ian', 'uan', 'uen', 'van', 'ang', 'eng', 'ing', 'ong', 'iang', 'iong', 'uang', 'ueng', 'b', 'd', 'g', 'm', 'n', 'l', 'j', 'zh', 'z', 'w', 'y', 'r', 'p', 't', 'k', 'f', 'h', 'x', 'ch', 'sh', 'c', 's'];
 $ret = '';
-$ret .= generatePhones('LL', '%s^*', $phoneMap) . "\r\n";
-$ret .= generatePhoneList('LL', '%s^*', $genMap) . "\r\n\r\n";
-$ret .= generatePhones('L', '*^%s-*', $phoneMap) . "\r\n";
-$ret .= generatePhoneList('L', '*^%s-*', $genMap) . "\r\n\r\n";
-$ret .= generatePhones('C', '*-%s+*', $phoneMap) . "\r\n";
-$ret .= generatePhoneList('C', '*-%s+*', $genMap) . "\r\n\r\n";
-$ret .= generatePhones('R', '*+%s=*', $phoneMap) . "\r\n";
-$ret .= generatePhoneList('R', '*+%s=*', $genMap) . "\r\n\r\n";
-$ret .= generatePhones('RR', '*=%s_*', $phoneMap) . "\r\n";
+$ret .= generatePhones('LL', '%s^*', $phoneMap) . "\n";
+$ret .= generatePhoneList('LL', '%s^*', $genMap) . "\n\n";
+$ret .= generatePhones('L', '*^%s-*', $phoneMap) . "\n";
+$ret .= generatePhoneList('L', '*^%s-*', $genMap) . "\n\n";
+$ret .= generatePhones('C', '*-%s+*', $phoneMap) . "\n";
+$ret .= generatePhoneList('C', '*-%s+*', $genMap) . "\n\n";
+$ret .= generatePhones('R', '*+%s=*', $phoneMap) . "\n";
+$ret .= generatePhoneList('R', '*+%s=*', $genMap) . "\n\n";
+$ret .= generatePhones('RR', '*=%s_*', $phoneMap) . "\n";
 $ret .= generatePhoneList('RR', '*=%s_*', $genMap);
 file_put_contents('test_qst.hed', $ret);
